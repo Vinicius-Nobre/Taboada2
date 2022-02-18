69155ca0343eaea3f39393983db2d1578452c592
TI <TI@DESKTOP-5S6PD6V>
2022-02-18 16:15:56 -0300

    Adicionar arquivos de projeto.

---
+++ Taboada2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Taboada2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=1;
+            int b=1;
+            int resultado;
+            int cont = 0;
+
+
+            for (int x = 1; x <= 101; x++)
+            {
+                resultado = a * b;
+
+                Console.WriteLine(a + "x" + b + "=" + resultado);
+
+                b = b+1;
+
+                cont = cont + 1;
+
+                if (cont == 11)
+                {
+
+                    Console.WriteLine();
+                    a = a+1;
+                    b = 1;
+                    cont = 1;
+                }
+               
+
+            }
+        }
+    }
+}
